7eb0b973d6f9f67dd3297f353604c0cdfbd3b348
ssamgir <amitbobade2007@gmail.com>
2016-08-28 01:04:57 +0530

    first commit

---
+++ SSSimpleAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSSimpleAnimation
+//
+//  Created by Amit Bobade on 28/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SSSimpleAnimation/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  SSSimpleAnimation
+//
+//  Created by Amit Bobade on 28/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+    NSArray *imageNames = @[@"win_1.png", @"win_2.png", @"win_3.png", @"win_4.png",
+                            @"win_5.png", @"win_6.png", @"win_7.png", @"win_8.png",
+                            @"win_9.png", @"win_10.png", @"win_11.png", @"win_12.png",
+                            @"win_13.png", @"win_14.png", @"win_15.png", @"win_16.png"];
+    
+    NSMutableArray *images = [[NSMutableArray alloc] init];
+    for (int i = 0; i < imageNames.count; i++) {
+        [images addObject:[UIImage imageNamed:[imageNames objectAtIndex:i]]];
+    }
+    
+    // Normal Animation
+    UIImageView *animationImageView = [[UIImageView alloc] initWithFrame:CGRectMake(60, 95, 86, 193)];
+    animationImageView.animationImages = images;
+    animationImageView.animationDuration = 0.5;
+    
+    [self.view addSubview:animationImageView];
+    [animationImageView startAnimating];
+
+    UIImageView *slowAnimationImageView = [[UIImageView alloc] initWithFrame:CGRectMake(160, 95, 86, 193)];
+    slowAnimationImageView.animationImages = images;
+    slowAnimationImageView.animationDuration = 5;
+    
+    [self.view addSubview:slowAnimationImageView];
+    [slowAnimationImageView startAnimating];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
